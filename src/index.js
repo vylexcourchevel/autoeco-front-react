68366a09253dc67e61663a298ee2ce94f02de050
John Doe <vylex@hotmail.fr>
2024-07-25 15:41:15 +0200

    ajout images

--- src/index.js
+++ src/index.js
@@ -6,11 +6,17 @@ import ReactDOM from 'react-dom';
 import App from './App'; // Assurez-vous que le chemin est correct
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import DefaultLayout from './components/DefaultLayout';
 
 
  ReactDOM.render(
+
+  <DefaultLayout>
+  
   <Provider store={store}>
     <App />
   </Provider>,
+
+  </DefaultLayout>,
   document.getElementById('root')
 )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,16 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { FETCH_SUCCESS, FETCH_FAILURE } from '../redux/reducers/sliceCar';  // Corrigez ce chemin
+import { FETCH_SUCCESS, FETCH_FAILURE } from '../redux/reducers/sliceCar'; // Corrigez ce chemin si nécessaire
 import axios from 'axios';
-
 
 const Home = () => {
   const dispatch = useDispatch();
-   
   const cars = useSelector((state) => state.car.data);
-  console.log(cars);
-  // const loading = useSelector((state) => state.car.data);
-  // const error = useSelector((state) => state.cars.error);
 
   useEffect(() => {
     const fetchCar = async () => {
-     
-     
       try {
         const { data } = await axios.get("http://localhost:8000/api/cars/all");
-       
-       
         dispatch(FETCH_SUCCESS(data));
       } catch (error) {
         dispatch(FETCH_FAILURE(error.message));
@@ -30,28 +21,27 @@ const Home = () => {
     fetchCar();
   }, [dispatch]);
 
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
-
-  // if (error) {
-  //   return <div>Error: {error}</div>;
-  // }
-
   return (
-    <div>
-     {cars.map((car, index) => (
-        <div key={index}>
-          <h2>{car.brand}</h2>
-         
-        </div>
-      ))}
+    <div className="container">
+      <div className="row">
+        {cars.map((car, index) => (
+          <div key={index} className="col-md-4 mb-4">
+            <div className="card" style={{ width: '18rem' }}>
+              <img src={car.image} className="card-img-top" alt={`${car.brand} ${car.model}`} />
+              <div className="card-body">
+                <h5 className="card-title">{car.brand} {car.model}</h5>
+                <p className="card-text">Registration Plate: {car.registrationPlate}</p>
+                <p className="card-text">Year: {car.years}</p>
+                <p className="card-text">Price per Day: {car.pricePerDay}</p>
+                <p className="card-text">Available: {car.available ? 'Yes' : 'No'}</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-      
-
   );
 };
 
 export default Home;
-
-
